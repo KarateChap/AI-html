534129f05e40aec16f72a3eff04223ec32e40379
Moose <gabrielmarianoofficial@gmail.com>
2022-09-15 20:09:25 +0800

    added changes to upload image

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const dropArea = document.querySelector(".drag-area"),
   input = dropArea.querySelector("input")
 let file //this is a global variable and we'll use it inside multiple functions
 const button = document.getElementById("btn")
+const imageRight = document.getElementById("img-right")
+const upload = document.getElementById("upload")
+const uploadText = document.getElementById("upload-text")
+
 button.onclick = () => {
   input.click() //if user click on the button then the input also clicked
 }
+upload.onclick = () => {
+  if (!fileURL) {
+    alert("Please browse an image first")
+  } else {
+    imageRight.src = fileURL
+    uploadText.innerHTML = "You uploaded:"
+  }
+}
+
 input.addEventListener("change", function () {
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = this.files[0]
@@ -32,6 +45,7 @@ dropArea.addEventListener("drop", (event) => {
   file = event.dataTransfer.files[0]
   showFile() //calling function
 })
+
 function showFile() {
   let fileType = file.type //getting selected file type
   let validExtensions = ["image/jpeg", "image/jpg", "image/png"] //adding some valid image extensions in array
@@ -39,7 +53,7 @@ function showFile() {
     //if user selected file is an image file
     let fileReader = new FileReader() //creating new FileReader object
     fileReader.onload = () => {
-      let fileURL = fileReader.result //passing user file source in fileURL variable
+      fileURL = fileReader.result //passing user file source in fileURL variable
       // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
       let imgTag = `<img src="${fileURL}" alt="image">` //creating an img tag and passing user selected file source inside src attribute
       dropArea.innerHTML = imgTag //adding that created img tag inside dropArea container
